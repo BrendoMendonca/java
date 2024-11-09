c439787ace6bfa512aeed3bed790011dc3e59300
unknown <brendomendonca20@gmail.com>
2024-11-08 21:51:56 -0300

    enviando primeiro codigo java

---
+++ arquivos/CursoJava.java
@@ -0,0 +1,9 @@
+public class CursoJava{
+    public static void main(String[] args){
+        String sobrenome = "Mendonça";
+        int idade = 25;
+        System.out.println("Alo mundo");//Pula linha
+        System.out.print("Alo mundo\n");//não pula linha
+        System.out.printf("Meu nome é Brendo %s e tenho %d anos", sobrenome, idade);
+    }
+}
